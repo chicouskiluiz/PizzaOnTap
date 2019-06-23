49919566282bc6bbcc38ea60aa675e5b81a09648
SUdoWinchester <luizeduardo2607@gmail.com>
2019-06-23 19:59:21 -0300

    Migration e model de endereço

---
+++ database/migrations/2019_06_23_223828_create_adresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('cep');
+            $table->string('address');
+            $table->string('number');
+            $table->string('neighborhood');
+            $table->string('complement');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adresses');
+    }
+}

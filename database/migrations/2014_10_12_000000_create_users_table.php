c5ed4d768184d7cb8c0506a6966ec1e236825d96
SUdoWinchester <luizeduardo2607@gmail.com>
2019-06-23 19:26:29 -0300

    Update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('cpf', 11)->unique();
+            $table->string('phone');
             $table->boolean('isAdmin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

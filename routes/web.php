086eaccfc66f0fcfee50835149f44a010ff35896
SUdoWinchester <luizeduardo2607@gmail.com>
2019-06-23 21:46:31 -0300

    Route update

--- routes/web.php
+++ routes/web.php
@@ -27,26 +27,32 @@ Route::get('/admin', 'HomeController@index')->name('pedidos');
 Route::get('/user/list', 'UserController@list');
 Route::post('/register', 'UserController@register')->name('user.register');
 
-Route::get('/admin', 'AdminController@index');
-Route::get('/admin/pizzas', 'AdminController@getPizzas');
-Route::get('/admin/sabores', 'AdminController@getSabores');
-Route::get('/admin/refrigerantes', 'AdminController@getRefrigerantes');
-Route::get('/admin/adicionais', 'AdminController@getAdicionais');
-Route::get('/admin/tamanhos', 'AdminController@getTamanhos');
-Route::get('/admin/usuarios', 'AdminController@getUsuarios');
+Route::middleware(['isAdmin'])->group(function () {
+    Route::get('/admin', 'AdminController@index');
+    Route::get('/admin/pizzas', 'AdminController@getPizzas');
+    Route::get('/admin/sabores', 'AdminController@getSabores');
+    Route::get('/admin/refrigerantes', 'AdminController@getRefrigerantes');
+    Route::get('/admin/adicionais', 'AdminController@getAdicionais');
+    Route::get('/admin/tamanhos', 'AdminController@getTamanhos');
+    Route::get('/admin/usuarios', 'AdminController@getUsuarios');
+});
 
 Route::get('/additional', 'AdditionalController@list');
 Route::post('/additional', 'AdditionalController@store');
+Route::put('/additional/{id}', 'AdditionalController@update');
 Route::delete('/additional/{id}', 'AdditionalController@delete');
 
 Route::get('/drink', 'DrinkController@list');
 Route::post('/drink', 'DrinkController@store');
+Route::put('/drink/{id}', 'DrinkController@update');
 Route::delete('/drink/{id}', 'DrinkController@delete');
 
 Route::get('/flavor', 'FlavorController@list');
 Route::post('/flavor', 'FlavorController@store');
+Route::put('/flavor/{id}', 'FlavorController@update');
 Route::delete('/flavor/{id}', 'FlavorController@delete');
 
 Route::get('/size', 'SizeController@list');
 Route::post('/size', 'SizeController@store');
+Route::put('/size/{id}', 'SizeController@update');
 Route::delete('/size/{id}', 'SizeController@delete');

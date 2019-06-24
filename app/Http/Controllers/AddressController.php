a9dabfc93923f83e4620d5ba5be344ee31b9c5ac
SUdoWinchester <luizeduardo2607@gmail.com>
2019-06-24 11:18:07 -0300

    endereço

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use App\Model\Adress;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AddressController extends Controller
 {
     public function get() {
-        return Adress::find(Auth::user()->id);
+        return DB::table('adresses')->where('user_id', Auth::user()->id)->get();
     }
 }
